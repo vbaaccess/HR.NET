16d6d4a214782f1bb6bc5d94c245c408e7083171
koter <pablodd@wp.pl>
2021-09-21 13:15:54 +0200

    EmployeeAddForm; Check Validate , Save new Employee (start)

--- UserInterface/Forms/Employees/EmployeeAddForm.cs
+++ UserInterface/Forms/Employees/EmployeeAddForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SystemHR.DataAccessLayer.Models;
 using SystemHR.DataAccessLayer.Models.Dictionaries;
 using SystemHR.UserInterface.Extensions;
 using SystemHR.UserInterface.Forms.Base;
@@ -62,17 +63,128 @@ namespace SystemHR.UserInterface.Forms.Employees
         protected override void Save()
         {
             base.Save(); // w klasie bazowej jest pusto, wiec mozna to usunac
-            MessageBox.Show("Zapisano", "UNDER CONSTRUCTION");
-            Close();
+            
+            if (ValidateForm())
+            {
+                EmployeeModel employee = new EmployeeModel()
+                {
+                        LastName = txtLastName.Text
+                    ,   Firstname = txtFirstName.Text
+                    ,   Gender = new GenderModel(cbGender.Text)
+                    ,   DateBirth = dtpDateBirth.Value
+                    ,   PESEL = txtPesel.Text
+                    ,   PhoneNumber = txtPhoneNumber.Text
+                    ,   EmailAddress = txtEmailAddress.Text
+                    ,   IdentityCardNumber = txtIdentityCardNumber.Text
+                    ,   IssueDateIdentityCard = dtpIssueDateIdentityCard.Value
+                    ,   ExpirationDateIdentityCard = dtpExpirationDateIdenitityCard.Value
+                    ,   PassportNumber = txtPassportNumber.Text
+                    ,   IssueDatePassport = dtpIssueDatePassport.Value
+                    ,   ExpirationDatePassport = dtpExpirationDatePassport.Value
+                    ,   Status = new StatusModel("Wprowadzony")
+                };
+
+                employee = CreateEmployee(employee);
+
+                /* TO DO 
+                 * Niestandardowe zdarzenia i delegacje.
+                 * ReloadEmployees();
+                 */
+
+                Close();
+            }
+            
+        }
+
+        private EmployeeModel CreateEmployee(EmployeeModel employee)
+        {
+            //TO DO - save to DataBase
+            employee.Id = 4;
+            employee.Code = 4;
+
+            return employee;
+        }
+
+        private bool ValidateForm()
+        {
+
+            bool ValidationCheckResult = true;
+
+            #region MessageBox Parameters
+            string sMsCaption = "Dodawanie pracownika";
+            StringBuilder sbMessage = new StringBuilder();
+
+            DialogResult msAnswer = DialogResult.Yes;               // it's means everything is OK , you can start save
+            MessageBoxButtons msButtons = MessageBoxButtons.YesNo;  // default
+            MessageBoxIcon msIcon = MessageBoxIcon.Information;     // default
+            #endregion
+
+            #region MessageBox Errora Cheking
+            // string - jest immutowalny (niezmienny) przy "zmianie" tworzymy tak naprawde nowy obiekt na stercie pamieci
+            // StringBuilder- reprezentuje modyfikowalny ciag znakow
+            StringBuilder sbErrorMessage = new StringBuilder();
+
+            string lastNameErrorMessage = epLastName.GetError(txtLastName);
+
+            if (!string.IsNullOrEmpty(lastNameErrorMessage))
+                sbErrorMessage.Append(Environment.NewLine + lastNameErrorMessage);
+
+            string firsNameErrorMessage = epFirstName.GetError(txtFirstName);
+            if (!string.IsNullOrEmpty(firsNameErrorMessage))
+                sbErrorMessage.Append(Environment.NewLine + firsNameErrorMessage);
+
+            if (sbErrorMessage.Length>0)
+            {
+                sbErrorMessage.Insert(0, "Uzupelnij Dane:");
+                sbMessage.Insert(0,sbErrorMessage.ToString());
+
+                msButtons = MessageBoxButtons.OK;
+                msIcon = MessageBoxIcon.Error;
+                ValidationCheckResult = false;
+            }
+            #endregion
+
+            #region MessageBox Warning To show
+            string strWarningMessage = epPESEL.GetError(txtPesel);
+
+            if (!string.IsNullOrEmpty(strWarningMessage))
+            {
+                sbMessage.Append(Environment.NewLine + strWarningMessage);
+                if (sbErrorMessage.Length == 0)
+                {
+                    sbMessage.Append(Environment.NewLine + "Czy mimo to chcesz dodac pracownika?");
+                    msButtons = MessageBoxButtons.YesNo;
+                    msIcon = MessageBoxIcon.Warning;
+                }   
+            }
+            #endregion
+
+            #region Build complet Message
+            //if (!string.IsNullOrEmpty(sbMessage.ToString()))
+            if (sbMessage.Length>0)
+            {
+                msAnswer =
+                MessageBox.Show(sbMessage.ToString()
+                               , sMsCaption
+                               , msButtons
+                               , msIcon
+                               );
+                if (msAnswer == DialogResult.Yes)
+                    ValidationCheckResult = true;
+                else
+                    ValidationCheckResult = false;
+
+            }
+            #endregion
+
+            return ValidationCheckResult;
         }
 
         protected override void Cancel()
         {
             base.Cancel(); // w klasie bazowej mozna dodac dodatkowe dzialanie dla wszystkich klas pochodnych
                            // w zaleznosci od potrzeby moze ona nastapic przed lub po wywolaniu dodatkwych funkcjonalnosci
-            MessageBox.Show("Anulowano", "UNDER CONSTRUCTION");
             Close();
-
         }
 
         private void dtp_ValueChanged(object sender, EventArgs e)
