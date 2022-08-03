f6ebf1d3ea9e2099fc59c8e8fe661cce8b9f7c48
pawel <pablodd@wp.pl>
2022-08-03 14:11:51 +0200

    Variable Declaration

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,13 +4,17 @@ namespace ConsoleApp
 {
     class Program
     {
+        static string globalVarialbe = "Zmienna globalna";
+
         //static void Main(string[] args)
         static void Main()
         {
             Console.Title = "C# How To";
             MainArgs();
+            PrintDataTypes();
 
             Console.ReadLine();
+            
         }
 
         // Basic Consol IO
@@ -30,6 +34,36 @@ namespace ConsoleApp
             } else
                 Console.WriteLine($"not given");
 
+        }
+
+        // Basic Data Types
+        static void PrintDataTypes()
+        {
+            LocalVarDeclaration();
+            Console.WriteLine($"\nglobalVarialbe: {globalVarialbe}");
+            
+        }
+
+        static void LocalVarDeclaration()
+        {
+            Console.WriteLine("\nLocal variable from LocalVarDeclaration()");
+            int myInt = 123;
+            double myDouble = 34.546;
+            char myChar = 'X';
+            string myString = "Cial italia";
+
+            Console.WriteLine($" myInt: {myInt} \n myDouble: {myDouble} \n myChar: {myChar} \n myString: {myString} \n");
+
+            DateTime dt = new DateTime(2022, 08, 06);
+            Console.WriteLine($"    dt: {dt}");
+            dt = DateTime.Today;
+            Console.WriteLine($" Today: {dt}");
+            dt = DateTime.Now;
+            Console.WriteLine($"   Now: {dt}");
+
+            TimeSpan ts = new TimeSpan(14, 15, 49);
+            Console.WriteLine($"     TimeSpan: {ts}");
+            Console.WriteLine($" TotalMinutes: {ts.TotalMinutes}");
         }
     }
 }
