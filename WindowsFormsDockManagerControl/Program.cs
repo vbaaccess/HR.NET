c02eff7fa62ce485e73c08a3b227247e97dac1b7
pawel <pablodd@wp.pl>
2022-08-08 14:30:01 +0200

    Example MDIParent (WinForm)

--- WindowsFormsDockManagerControl/Program.cs
+++ WindowsFormsDockManagerControl/Program.cs
@@ -16,7 +16,8 @@ namespace WindowsFormsDockManagerControl
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+            Application.Run(new MDIParent());
         }
     }
 }
